a168b813a8e44b51c89f752e0f15ff03d85e81e0
Lauren Klein <leklein@calpoly.edu>
2017-04-06 20:40:46 -0700

    Added /courseDb and /item routes
    courseDb is a database of courses, item is for testing purposes.
    Neither currently persist as far as I know.

---
+++ src/main/java/base/courseDb/Course.java
@@ -0,0 +1,63 @@
+/**
+ * Created by Lauren on 4/6/2017.
+ */
+
+package base.courseDb;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Course implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private String prefix;
+    private String number;
+    private String title;
+
+    public Course (){};
+
+    public Course(String prefix, String number, String title) {
+        this.prefix = prefix;
+        this.number = number;
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
